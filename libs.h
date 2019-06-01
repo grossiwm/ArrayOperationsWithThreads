47172b2f7bd26e18c193b9d9fec32af0eb70c948
Gabriel Rossi Lopes <rossi.gabriel@outlook.com>
2019-06-01 01:49:49 -0300

    Header for include necessary libs.

---
+++ libs.h
@@ -0,0 +1,16 @@
+#ifndef THREADSFORARRAYHANDLING_LIBS_H
+#define THREADSFORARRAYHANDLING_LIBS_H
+
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "math.h"
+#include <stdbool.h>
+#include "functions.h"
+#include "4test.h"
+#include "definitions.h"
+#include "structures.h"
+#include "mallocs.h"
+
+#endif //THREADSFORARRAYHANDLING_LIBS_H
